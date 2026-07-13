10776f7947c42a59e67137ffbb8e502d2b4f6713
guopengbo.123 <guopengbo.123@bytedance.com>
2026-07-13 21:09:52 +0800

    deploy: publish Xinjiang road trip

---
+++ _next/static/chunks/0dbs9p5vpjwsm.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,39472,t=>{t.v(o=>Promise.all(["static/chunks/11~d_ak9at4o5.js"].map(o=>t.l(o))).then(()=>o(51414)))}]);
